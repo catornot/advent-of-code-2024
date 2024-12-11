171694795e2307e327d722272f8a23a9b53d72ae
catornot <41955154+catornot@users.noreply.github.com>
2024-12-10 20:25:43 -0500

    update readme and add day10 benchmark

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # advent-of-code-2024
 advent of code solutions for 2024
+
+# Usage
+
+files can be fetched with a python script
+
+`python fetch_file.py`
+
+- `cargo run -- {day} {part}` for running a single solution
+- `cargo bench --features all-benches` for benchmarking all solutions
+- `cargo bench bench{day} --features all-benches` for benchmarking a single day
+
+## nix
+cargo is expected to be preinstalled on the system
+
+a `shell.nix` exist for python
---
+++ benches/bench10.rs
@@ -0,0 +1,23 @@
+use daylibs::*;
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn part_1(bencher: divan::Bencher) {
+    bencher
+        .with_inputs(|| (Day10, get_input(10).unwrap()))
+        .bench_values(|(mut day, input)| {
+            day.part_1(divan::black_box(input));
+        });
+}
+
+#[divan::bench]
+fn part_2(bencher: divan::Bencher) {
+    bencher
+        .with_inputs(|| (Day10, get_input(10).unwrap()))
+        .bench_values(|(mut day, input)| {
+            day.part_2(divan::black_box(input));
+        });
+}
